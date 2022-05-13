ad54bf7e4302b569bf4f72673ea0cd17a4291762
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-13 12:29:39 +0200

    fix: fix function showSlide (Fri, May 13, 12:29:38 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -3,16 +3,21 @@ const slides = document.getElementsByClassName('slide');
 const dots = document.getElementsByClassName('dot');
 
 const showSlide = (number) => {
+    slideNumber = number;
     if (number > slides.length) {
         slideNumber = 0;
     }
+    if (number < 0) {
+        slideNumber = slides.length;
+    }
 
     for (let i = 0; i < slides.length; i++) {
-        if (i !== slideNumber) {
-            slides[i].classList.add('invisible');
+        console.log('slideNumber = ' + slideNumber);
+        if (i === slideNumber) {
+            slides[i].classList?.remove('invisible');
             dots[i].classList.add('active');
         } else {
-            slides[i].classList?.remove('invisible');
+            slides[i].classList.add('invisible');
             dots[i].classList?.remove('active');
         }
     }
