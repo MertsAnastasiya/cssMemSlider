1e0c7be5ce87fcd261f22bb33fe86ba7c783ade7
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-05-16 12:11:04 +0200

    feat: add animation for slide's text (Mon, May 16, 12:11:01)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,6 +1,23 @@
+const slideSources = [
+    {
+        img: './assets/images/mem1.jpeg',
+        text:`I'm ready to get hurt again`,
+    },
+    {
+        img: './assets/images/mem2.jpeg',
+        text:`Mem 2`,
+    },
+    {
+        img: './assets/images/mem3.jpeg',
+        text:`Mem 3`,
+    },
+];
+
 let slideNumber = 0;
 const slides = document.getElementsByClassName('slide');
 const dots = document.getElementsByClassName('dot');
+
+document.documentElement.style.setProperty('--textWidth', slideSources[0].text.length + 'ch');
 
 const showSlide = (number) => {
     slideNumber = number;
@@ -12,7 +29,9 @@ const showSlide = (number) => {
     }
 
     for (let i = 0; i < slides.length; i++) {
-        console.log('slideNumber = ' + slideNumber);
+        const textWidthCss = slides[i].children[1].innerHTML.length
+        document.documentElement.style.setProperty('--textWidth', textWidthCss + 'ch');
+        
         if (i === slideNumber) {
             slides[i].classList?.remove('invisible');
             dots[i].classList.add('active');
